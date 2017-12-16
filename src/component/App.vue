4c471ae215e6026d326e8bbb792928faf77442aa
OceanLy <github_ljj@126.com>
2017-12-16 10:51:27 +0800

    脚手架搭建

---
+++ src/component/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="box">
+     测试
+  </div>
+</template>
+
+<script>
+  export default {
+       data(){
+           return {}
+       }
+  }
+</script>
+
+<style scoped>
+    *{
+       margin: 0;
+       padding: 0;
+    }
+     .box{
+         width: 200px;
+         height: 240px;
+         border: 1px solid #000;
+         text-align: center;
+         background-color: green;
+     }
+</style>
